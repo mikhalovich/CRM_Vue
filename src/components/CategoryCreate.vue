94d7796494153d605e03e40f77beef1f243e661f
mikhalovich <zheny.mi@gmail.com>
2019-07-13 18:41:26 +0300

    Added validations to CategoryCreate form

--- src/components/CategoryCreate.vue
+++ src/components/CategoryCreate.vue
@@ -5,17 +5,37 @@
         <h4>Создать</h4>
       </div>
 
-      <form>
+      <form @submit.prevent="submitHandler">
         <div class="input-field">
-          <input id="name" type="text" />
+          <input
+            id="name"
+            type="text"
+            v-model="title"
+            :class="{invalid: $v.title.$dirty && !$v.title.required}"
+          />
           <label for="name">Название</label>
-          <span class="helper-text invalid">Введите название</span>
+          <span
+            class="helper-text invalid"
+            v-if="$v.title.$dirty && !$v.title.required"
+          >
+            Введите название категории
+          </span>
         </div>
 
         <div class="input-field">
-          <input id="limit" type="number" />
+          <input
+            id="limit"
+            type="number"
+            v-model="limit"
+            :class="{invalid: $v.limit.$dirty && !$v.limit.minValue}"
+          />
           <label for="limit">Лимит</label>
-          <span class="helper-text invalid">Минимальная величина</span>
+          <span
+            class="helper-text invalid"
+            v-if="$v.limit.$dirty && !$v.limit.minValue"
+          >
+            Минимальное значение {{ $v.limit.$params.minValue.min }}
+          </span>
         </div>
 
         <button class="btn waves-effect waves-light" type="submit">
@@ -26,3 +46,34 @@
     </div>
   </div>
 </template>
+
+<script>
+import { required, minValue } from 'vuelidate/lib/validators';
+
+export default {
+  data: () => ({
+    title: '',
+    limit: 100,
+  }),
+
+  validations: {
+    title: { required },
+    limit: { minValue: minValue(100) },
+  },
+
+  mounted() {
+    // eslint-disable-next-line
+    M.updateTextFields();
+  },
+
+  methods: {
+    submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        // eslint-disable-next-line
+        return;
+      }
+    },
+  },
+};
+</script>
