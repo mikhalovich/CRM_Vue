e563cfc74cae369ecf1ed84f95b7856abd4e7f48
mikhalovich <zheny.mi@gmail.com>
2019-07-17 13:06:38 +0300

    Connected tooltip derectives

---
+++ src/directives/tooltip.directive.js
@@ -0,0 +1,15 @@
+export default {
+  bind(el, { value }) {
+    // eslint-disable-next-line
+    M.Tooltip.init(el, { html: value });
+  },
+
+  unbind(el) {
+    // eslint-disable-next-line
+    const tooltip = M.Tooltip.getInstance(el);
+
+    if (tooltip && tooltip.destroy) {
+      tooltip.destroy();
+    }
+  },
+};
